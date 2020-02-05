44027ff01442a99641796327f3cebded794f7031
Hamza El Aouane <haouane@spartaglobal.com>
2020-02-05 00:04:25 +0000

    Added comments to the project

--- Guess_Number.py
+++ Guess_Number.py
@@ -1,19 +1,26 @@
 import random
 #======================================================
-#   VARIABLE FIELD
+#VARIABLE FIELD
+#STRUCTURE: THE GAME COSIST IN 3 DIFFICULT LEVELS
+#LEVEL 1 = RANGE BETWEEN 1 AND 10 AND 5 ATTEMPTS
+#LEVEL 2 = RANGE BETWEEN 1 AND 50 AND 10 ATTEMPTS
+#LEVEL 3 = RANGE BETWEEN 1 AND 100 AND 15 ATTEMPTS
 #======================================================
 secret_number_easy = random.randint(1, 10)
 secret_number_normal = random.randint(1, 50)
 secret_number_hard = random.randint(1,100)
 attempts_easy = 5
-attempts_normal = 20
-attempts_hard = 25
+attempts_normal = 10
+attempts_hard = 15
 count = 0
 
 name = input("Hello Sir/Madame, may i have your name please?: ")
 answer = input(f"Hello {name}, Welcome to 'Guess a Number', do you know how to play the game?(Y/N)")
 level = int(input("Please choose a level: "))
 
+#======================================================
+#CORE GAME CODE
+#======================================================
 if level == 1:
     for attempt in range(attempts_easy):
         count = count + 1
